bfc82802ffdbd0b2546c7789b1e00a74f2103003
csthink <security.2009@live.cn>
2019-08-22 20:39:21 +0800

    为样式添加 CSS3 前缀

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
             options: { sourceMap: true }
           },
           {
+            loader:"postcss-loader",
+            options: {
+              ident: "postcss",
+              sourceMap: true,
+              plugins: loader => [
+                require('autoprefixer')(),
+                // 这里可以使用更多配置，如上面提到的 postcss-cssnext 等
+                // require('postcss-cssnext')()
+              ]
+            }
+          },
+          {
             loader:"sass-loader",
             options:{ sourceMap: true }
           },
